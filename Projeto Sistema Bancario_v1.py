01f86cf74bbb5bcd9b5cc34aa61e7f3366878a22
Alexandre Júnior <dev.jralexandre@gmail.com>
2024-08-15 10:23:06 -0300

    Update Projeto Sistema Bancario_v1.py
    Feitos no trabalho ainda não

--- Projeto Sistema Bancario_v1.py
+++ Projeto Sistema Bancario_v1.py
@@ -1,8 +1,21 @@
+def depositos_(valor, saldo,/,):#valor e saldo
+        if  valor <= 0:
+            print("Valo Inválido")
+        else:
+            saldo += float(valor)
+            print("Valor depositado com sucesso!")
+        return saldo
+
+def saques_(*,saldo,valor,contador):
+        saldo -= float(valor)
+        contador += 1
+        return saldo,contador
+
+
 opcao = int
-saldo = 0.0
-cont_de_dep = 0
-depositos = {}
-saques = {}
+Saldo = 0.0
+depositos = []
+saques = []
 cont_de_saq = 0
 while True:
     menu_de_ops = f"""
@@ -17,40 +30,33 @@ MENU
     #Opção de deposito
     if opcao == 1:
             valor_de_deposito = int(input("Digite valor a ser depositado: R$ "))
-            if valor_de_deposito <= 0:
-                print("Valo Inválido")
-            else:
-                saldo += float(valor_de_deposito)
-                depositos[cont_de_dep]= f"R$ {valor_de_deposito},00"
-                cont_de_dep += 1
-                print("Valor depositado com sucesso!")
+            Saldo = depositos_(valor_de_deposito,Saldo)
+            depositos.append(f"R$ {valor_de_deposito}")
     #Opção de saque
     if opcao == 2:
-        while True:
-            valor_de_saque = int(input("Digite valor a ser sacado: R$ "))
-            if cont_de_saq == 3:
-                print("Número de saques excedido")
-                break
-            if valor_de_saque <= 0:
-                print("Valor Inválido")
-            else:
-                if valor_de_saque > saldo:
-                    print("Saldo insuficiente")
+            while True:
+                valor_de_saque = int(input("Digite valor de saque: R$ "))
+                if valor_de_saque <=0:
+                    print("Valor Inválido")
+                elif valor_de_saque > Saldo:
+                    print("Saldo Insuficiente")
                     break
                 else:
-                    saldo -= float(valor_de_saque)
-                    saques[cont_de_saq] = f"R$ {valor_de_saque},00"
-                    cont_de_saq += 1
-                    print("Saque realizado com sucesso")
+                    retorno = saques_(saldo = Saldo, valor = valor_de_saque,contador = cont_de_saq)
+                    Saldo = retorno[0]
+                    saques.append(f"R$ {float(valor_de_saque)} ")
+                    cont_de_saq = retorno[1]
                     break
+            
+            
     #Opção de extrato
     if opcao == 3:
         print("Extrato")
-        for i in range(0,cont_de_dep):
+        for i in  range(len(depositos)):
                 print(f"Despoito[{i+1}]: {depositos[i]}")
-        for i in range(0,cont_de_saq):
+        for i in range(len(saques)):
                 print(f"Saque[{i+1}]: {saques[i]}")
-        print(f"Saldo: R$ {saldo:.2f}")    
+        print(f"Saldo: R$ {Saldo}")    
     #Encerrar programa                
     if opcao ==0:
         break
